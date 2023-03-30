041e710442b24f4def6182c560ce7a2658aeeb95
sbenateau <sbenateau@mailodie.fr>
2023-03-30 13:37:10 +0200

    change order

--- fonction/compter_les_piafs.R
+++ fonction/compter_les_piafs.R
@@ -2,5 +2,5 @@ compter_les_piafs <- function(data_piafs) {
   data_piafs %>%
     group_by(NAME_SPECIES) %>%
     summarise(nb_occurence = n()) %>%
-    arrange(desc(nb_occurence))
+    arrange(nb_occurence)
 }
